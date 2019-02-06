216ae34b88497d7a80ad2fb3bf4e6216d65abb69
kévin geiger <geiger1230@gmail.com>
2019-02-06 15:11:20 +0100

    accueil style

--- functions.php
+++ functions.php
@@ -5,3 +5,13 @@ add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
 function wpchild_enqueue_styles(){
   wp_enqueue_style( 'wpm-wp-bootstrap-starter-style', get_template_directory_uri() . '/style.css' );
 }
+
+/*Ajout des menus*/
+function register_my_menus() {
+  register_nav_menus(
+  array(
+  'pre-header-menu' => __( 'Menu Pre Header' ),
+  )
+  );
+ }
+ add_action( 'init', 'register_my_menus' );
